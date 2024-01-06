8e05d29a25db9bf5614ea3453e62a4c20d2bec8d
thinkmetal-yash <yash@thinkmetal.in>
2024-01-06 12:02:47 +0530

    api working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express=require('express')
 const app=express()
-app.listen(5000,()=>{
+app.listen(80,()=>{
     console.log('Server running on port 5000')
 })
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hi peeks!</h1>')
+})
+
+app.get('/api',(req,res)=>{
+    res.json({message:"Hi this is api cmo"})
 })

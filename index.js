4e61dabfb1b37591db1443fa1d728c5d782a9826
thinkmetal-yash <yash@thinkmetal.in>
2024-01-06 12:56:39 +0530

    -error -temp

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express=require('express')
+const path = require('path')
 const app=express()
 app.listen(5000,()=>{
     console.log('Server running on port 5000')
 })
 
+app.use(express.static(path.join(__dirname, 'react-yb', 'build')))
 app.get('/',(req,res)=>{
     res.send('<h1>Hi peeks!</h1>')
 })

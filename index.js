ef2aa5403feaa11fc68655985a31f62c3f3c6f13
thinkmetal-yash <yash@thinkmetal.in>
2024-01-06 12:28:50 +0530

    this

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require('express')
 const app=express()
-app.listen(80,()=>{
+app.listen(5000,()=>{
     console.log('Server running on port 5000')
 })
 
@@ -10,4 +10,13 @@ app.get('/',(req,res)=>{
 
 app.get('/api',(req,res)=>{
     res.json({message:"Hi this is api cmo"})
+})
+app.get('/api/users',(req,res)=>{
+    res.json({
+        users:[
+            {
+                name:'yash bansal'
+            }
+        ]
+    })
 })
